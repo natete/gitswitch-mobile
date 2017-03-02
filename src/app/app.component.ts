814b725012c70b49f368c405c017e2a8d3e56c7d
natete <natete981@gmail.com>
2017-03-02 15:08:01 +0100

    Basic auth service and token management

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,24 @@ import { TabsPage } from '../pages/tabs/tabs';
 export class MyApp {
   rootPage = TabsPage;
 
-  constructor(platform: Platform) {
-    platform.ready()
+  constructor(private platform: Platform) {
+
+    this.initApp();
+
+    this.initLoginSubscription();
+  }
+
+  private initApp() {
+    this.platform.ready()
             .then(() => {
               // Okay, so the platform is ready and our plugins are available.
               // Here you can do any higher level native things you might need.
               StatusBar.styleDefault();
               Splashscreen.hide();
             });
+  }
+
+  initLoginSubscription() {
+
   }
 }
---
+++ src/providers/auth/auth-service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { TokenService } from './token-service';
+import { Subject, Observable } from 'rxjs';
+
+@Injectable()
+export class AuthService {
+
+  private authSubject = new Subject<void>();
+
+  constructor(private http: Http,
+              private tokenService: TokenService) { }
+
+  getAuthStream(): Observable<void> {
+    return this.authSubject.asObservable();
+  }
+
+  login(username: string, password: string): void {
+    // TODO: post a request to get the token
+    const token = '';
+
+    this.tokenService
+        .setToken(token)
+        .then(() => this.authSubject.next());
+  }
+
+  logout(): void {
+    this.tokenService.revokeToken();
+  }
+}
