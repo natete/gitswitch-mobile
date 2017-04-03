5bba7d2f16ce19a0382017fc624d318624625593
mavillalba <mavillalba@emergya.com>
2017-04-03 18:09:43 +0200

    Deleted commits, comments and added repository name.

--- src/pages/pull-requests/pull-request.ts
+++ src/pages/pull-requests/pull-request.ts
@@ -1,17 +1,17 @@
 export interface PullRequest {
   title: string;
-  description: string,
-  username: string,
-  date: string,
-  commits: number,
-  comments: number,
-  id: number,
-  from: string,
-  to: string,
-  from_repo_id: number,
-  from_repo_name: string,
-  to_repo_id: number,
-  to_repo_name: string,
-  type: string,
-  updated: string
+  description: string;
+  username: string;
+  date: string;
+  commits: number;
+  comments: number;
+  id: number;
+  from: string;
+  to: string;
+  from_repo_id: number;
+  from_repo_name: string;
+  to_repo_id: number;
+  to_repo_name: string;
+  type: string;
+  updated: string;
 }
